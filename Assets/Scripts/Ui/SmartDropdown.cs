2cef7629b7dcf371dfda4faa64dc434df815b6f9
alukens <alexlukens@outlook.com>
2021-06-11 19:10:44 -0400

    Admin Bar + Save System + Drop Down Menu

---
+++ Assets/Scripts/Ui/SmartDropdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartDropdown : MonoBehaviour
+{
+    [Header("Status")]
+    public bool storageCrateOpen;
+
+    [Header("Setup")]
+    public GameObject dropDownBtn;
+    public Transform parentDropDown;
+
+    public static SmartDropdown instance;
+    [HideInInspector]
+    public GameObject smartDropdown;
+
+    private void Awake()
+    {
+        instance = this;
+        smartDropdown = gameObject;
+    }
+
+
+}
