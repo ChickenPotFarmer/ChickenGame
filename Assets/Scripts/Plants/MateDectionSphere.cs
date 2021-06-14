0198e8860a19410515567e312a9c9efa27f955b9
alukens <alexlukens@outlook.com>
2021-06-13 22:09:28 -0400

    Mate Detection

---
+++ Assets/Scripts/Plants/MateDectionSphere.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MateDectionSphere : MonoBehaviour
+{
+    public List<WeedPlant> matesInRange;
+    public WeedPlant parentPlant;
+
+    private void Start()
+    {
+        if (!parentPlant)
+            parentPlant = GetComponentInParent<WeedPlant>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weed Plant"))
+        {
+            WeedPlant inRangePlant = other.GetComponent<WeedPlant>();
+
+            if (inRangePlant != parentPlant)
+                matesInRange.Add(other.GetComponent<WeedPlant>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Weed Plant"))
+        {
+            matesInRange.Remove(other.GetComponent<WeedPlant>());
+        }
+    }
+}
